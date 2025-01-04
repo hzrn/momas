857fef8b17ed8b3219ae79514454a4b71d9f6826
Hazrin <mohazmad01@gmail.com>
2025-01-05 06:11:46 +0800

    `Update redis configuration in database.php`

--- config/database.php
+++ config/database.php
@@ -119,28 +119,28 @@ return [
     |
     */
 
-    'redis' => [
-        'client' => env('REDIS_CLIENT', 'predis'),
-
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'predis'),
-            'prefix' => env('REDIS_PREFIX', 'laravel_database_'),
-        ],
-
-        'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 0),
-        ],
-
-        'cache' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_CACHE_DB', 1),
-        ],
+'redis' => [
+    'client' => env('REDIS_CLIENT', 'predis'), // This should be 'predis' if you're using the Predis package.
+    'options' => [
+        'cluster' => env('REDIS_CLUSTER', 'predis'),
+        'prefix' => env('REDIS_PREFIX', 'laravel_database_'),
     ],
+
+    'default' => [
+        'host' => env('REDIS_HOST', '127.0.0.1'),
+        'password' => env('REDIS_PASSWORD', null),
+        'port' => env('REDIS_PORT', 6379),
+        'database' => env('REDIS_DB', 0),
+    ],
+
+    'cache' => [
+        'host' => env('REDIS_HOST', '127.0.0.1'),
+        'password' => env('REDIS_PASSWORD', null),
+        'port' => env('REDIS_PORT', 6379),
+        'database' => env('REDIS_CACHE_DB', 1),
+    ],
+],
+
 
 
 ];
