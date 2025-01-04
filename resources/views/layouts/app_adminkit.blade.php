874d404c562286bc4e6f50bb6f52e8be4691a818
Hazrin <mohazmad01@gmail.com>
2025-01-05 01:22:45 +0800

    `Added conditional flash message display in app_adminkit layout`

--- resources/views/layouts/app_adminkit.blade.php
+++ resources/views/layouts/app_adminkit.blade.php
@@ -222,9 +222,15 @@
 
             <main class="content">
                 <div class="container-fluid p-0">
-                    @include('flash::message')
+                    @if (session('flash'))
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            {{ session('flash') }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    @endif
                     @yield('content')
                 </div>
+
             </main>
         </div>
 
