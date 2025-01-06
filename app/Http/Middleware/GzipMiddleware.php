8dd9e9ce03f0765487fd04dfe7dd7c30909a54ce
Hazrin <mohazmad01@gmail.com>
2025-01-06 16:56:03 +0800

    `Update GzipMiddleware to skip gzip for BinaryFileResponse and update Content-Length header`

--- app/Http/Middleware/GzipMiddleware.php
+++ app/Http/Middleware/GzipMiddleware.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class GzipMiddleware
 {
@@ -10,12 +10,27 @@ class GzipMiddleware
     {
         $response = $next($request);
 
+        // Check if the response is a BinaryFileResponse
+        if ($response instanceof BinaryFileResponse) {
+            return $response; // Skip gzip for file responses
+        }
+
+        // Check if the client accepts gzip encoding
         if (strpos($request->header('Accept-Encoding'), 'gzip') !== false) {
+            // Set the Content-Encoding header
             $response->header('Content-Encoding', 'gzip');
-            $response->setContent(gzencode($response->getContent(), 6));
+
+            // Compress the content
+            $content = $response->getContent();
+            $compressedContent = gzencode($content, 6);
+
+            // Set the compressed content
+            $response->setContent($compressedContent);
+
+            // Update the Content-Length header
+            $response->header('Content-Length', strlen($compressedContent));
         }
 
         return $response;
     }
 }
-
